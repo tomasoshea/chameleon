895ea4974ce3d2acd6a541034e9e0b9f2d3d075d
tomasoshea <t.goshea@yahoo.co.uk>
2023-05-09 09:54:10 +0200

    started on cham3

--- cham3.cpp
+++ cham3.cpp
@@ -35,21 +35,6 @@ double T2eV = 2e-16 * 1e18; // Tesla to eV2
 double z3 = 1.202056903159594;  // Riemann zeta(3) 
 double Lam = 1e-3;    // cosmological constant [eV] 2.4e-3
 
-// solar params
-double R = 149.5978707e9;	// mean earth-sun distance [m]
-double rSolar = 6.9598E+10;	// solar radius [m]
-double xt = 0.7;    //radial location of tachocline [frac]
-double Dx = 0.01;   // tachocline thickness [frac]
-double rt = xt * rSolar;   // radial location of tachocline [m]
-double Dr = Dx * rSolar;  // tachocline thickness [m]
-double Bt = 30 * T2eV;    // tachocline B-field [eV2]
-double rho = 891921.86598 * 1e12; // tachocline density [eV4]
-double T = 0.1995759863 * 1e3;  // tachocline temperature [eV]
-double ne = 1.06051586e+23 * 1e6 * pow(m2eV,3); // e- number density [eV3]
-double mfp = 0.003;  // photon mean free path [m]
-double ng = 1e21 * 1e4;  // tachocline photon flux [m-2 s-1]
-double wp2 = 4*pi*a*ne/(m_e); // plasma frequency squared [eV2]
-
 // CAST params
 //double L = 9.26; // CAST length [m]
 //double B = 9 * T2eV;    // CAST B-field [eV2]
@@ -96,7 +81,7 @@ void write2D( string name, vector<double> data1, vector<double> data2) {
 }
 
 // chameleon mass squared
-double mCham( double w, double n, double Bm ){
+double mCham( double w, double n, double Bm, double rho ){
     // calculate chameleon mass squared [eV2]
     double wd2 = ( (n+1) * rho / mpl ) 
     * pow( rho / (n * mpl * pow( Lam, (n+4) ) ) , ( 1 / (n+1) ) );
@@ -107,23 +92,23 @@ double mCham( double w, double n, double Bm ){
 }
 
 // symmetron mass squared (for high density approx) [eV2]
-double mSym( double Bm ){
+double mSym( double Bm, double rho ){
     return Bm * rho / pow(mpl,2);
 }
 
 // density dependant dilaton (Brax) for negligible Vc [eV2]
-double mDil( double Bm ){
+double mDil( double Bm, double rho ){
     return Bm * rho / pow(mpl,2);
 }
 
 // coherence length [eV-1]
-double lom( double w, double n, double Bm ) {
+double lom( double w, double n, double Bm, double rho, double wp ) {
     
     // get m2 from corresponding theory
-    double m2 = mCham( w, n, Bm );
+    double m2 = mCham( w, n, Bm, rho );
 
     // calculate coherence length (pos def)
-    double item = 4 * w / sqrt( pow( m2 - wp2, 2 ));
+    double item = 4 * w / sqrt( pow( m2 - pow(wp,2) , 2 ));
     return item;
 }
 
@@ -133,37 +118,17 @@ double I( double a ) {
 }
 
 // tachocline photon distribution [eV-1]
-double pg( double w ) {
+double pg( double w, double T ) {
     return pow(w,2) / ( ( 2 * z3 * pow(T,3) ) * ( exp(w/T) - 1 ) );
 }
 
-/*
-// phi from dw integral
-double wIntegral( double n, double Bm ) {
-
-    // integrate wrt w over CAST energies (0.5 - 15 keV) by trapezia
-    double dw = 1e1;
-    double item = 0.;
-    for( double w = 5e2; w < 15e3; w+=dw) {
-
-        double l = lw(w,n,Bm);
-        double p1 = ng * pg(w) * sqrt(ls/l) * pow(rSolar,3) * pow( B * Bt * l * xt / ( 4 * mpl*mpl * R ), 2 ) * Dx * I(l/mfp) / mfp;
-        l = lw(w+dw,n,Bm);
-        double p2 = ng * pg(w+dw) * sqrt(ls/l) * pow(rSolar,3) * pow( B * Bt * l * xt / ( 4 * mpl*mpl * R ), 2 ) * Dx * I(l/mfp) / mfp;
-
-        item += ( dw * (p1+p2) / 2 );
-    }
-
-    return item;
-}
-*/
-
 
 // differential flux emitted from sun [m-2 s-1 eV-1]
-double solarFlux( double w, double n, double Bm ) {
+double solarFlux( double w, double n, double Bm, double rho, double wp ) {
 
-    double lw = lom(w,n,Bm);  // [eV-1]
-    return ng * pg(w) * (Dr / mfp) * pow( Bt * lw / mpl*2, 2 ) * sqrt(ls/lw*hbarc) * I(lw*hbarc/mfp);
+    double lw = lom( w, n, Bm, rho, wp );  // [eV-1]
+    return ng * pg(w) * (Dr / mfp) * pow( Bt * lw / mpl*2, 2 )
+             * sqrt(ls/lw*hbarc) * I(lw*hbarc/mfp);
 }
 
 
@@ -183,8 +148,20 @@ double wIntegral( double n, double Bm ) {
 // calculate limits
 int main(){
 
+    vector<double> r = read("data/r.dat");	// sun radial distance [eV-1]
+	vector<double> T = read("data/T.dat");	// solar temperature [eV]
+	vector<double> ne = read("data/ne.dat");	// electron number density [eV3]
+	vector<double> nH = read("data/nH.dat");	// H ion density [eV3]
+	vector<double> nHe4 = read("data/nHe4.dat");	// He4 ion density [eV3]
+	vector<double> nHe3 = read("data/nHe3.dat");	// He3 ion density [eV3]
+	
+	// get gaunt factors
+	vector<vector<double>> z1 = readGaunt("data/Z1.dat");	// gaunt factors for Z=1
+	vector<vector<double>> z2 = readGaunt("data/Z2.dat");	// gaunt factors for Z=2
+
+
     // set model parameter n
-    double n = 10;
+    double n = 1;
     // initialise vectors
     vector<double> BgVec;
     vector<double> BmVec;
@@ -202,5 +179,3 @@ int main(){
     //write2D( path + ext, BmVec, BgVec );
     return 0;
 }
-
-// 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
